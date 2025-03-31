933fd38f0ebe6cd82ce9362cedf7e135776e3750
Jorge Caldeira <caldeirajorge03@gmail.com>
2025-04-01 00:24:03 +0100

    voltamos a csv mas agr faz um de append e varios por instancias diferente just in case

--- nonattrserver.js
+++ nonattrserver.js
@@ -1,33 +1,36 @@
-import express, { Request, Response } from "express";
-
-if (!process.env.D1_DATABASE_URL) {
-    throw new Error("Environment variable D1_DATABASE_URL is not set.");
-}
-
-
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 //const express = require("express");
 const cors = require("cors"); // Import the CORS package
 const fs = require("fs");
 const path = require("path");
 const AWS = require("aws-sdk");
-const {D1Database} = require("@cloudflare/d1");
-//const express1 = require("express");
+const { D1Database } = require("@cloudflare/d1");
 require("dotenv").config();
-
-const app = express();
+const app = (0, express_1.default)();
 app.use(cors()); // Enable CORS for all routes
-app.use(express.json()); // Middleware to parse JSON data
-
-const db = new D1Database(process.env.D1_DATABASE_URL)
+app.use(express_1.default.json()); // Middleware to parse JSON data
+const db = new D1Database(process.env.D1_DATABASE_URL);
 // Root route for testing
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
     res.send("Backend is up and running!");
 });
-
 // Endpoint to save quiz results
-app.post("/save-quiz", async (req: Request, res: Response): Promise<void> => {
+app.post("/save-quiz", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { email, score, time } = req.body;
-
     if (!email || typeof score !== "number" || typeof time !== "number") {
         res.status(400).send("Invalid data.");
         return;
@@ -36,17 +39,15 @@ app.post("/save-quiz", async (req: Request, res: Response): Promise<void> => {
     INSERT INTO quiz_diario_sds (email, score, time)
     VALUES (?, ?, ?);
 `;
-
     try {
-        await db.prepare(sqlQuery).bind(email, score, time).run();
+        yield db.prepare(sqlQuery).bind(email, score, time).run();
         console.log("Data saved successfully!");
         res.status(200).send("Data saved successfully!");
-    } catch (err) {
+    }
+    catch (err) {
         console.error("Error saving data to D1 database:", err);
         res.status(500).send("Failed to save data.");
     }
-
-
     /* fileName = `quiz_${new Date().toISOString().split("T")[0]}.csv`;
     const filePath = path.join(__dirname, fileName);
 
@@ -62,14 +63,12 @@ app.post("/save-quiz", async (req: Request, res: Response): Promise<void> => {
         console.log("Data saved successfully!");
         res.status(200).send("Data saved successfully!");
     });*/
-});
-
+}));
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
 /*const s3 = new AWS.S3({
     endpoint: "https://1d5bedc6e06a996c3ca93b5e69f491de.r2.cloudflarestorage.com/quizzes-sds",
     accessKeyId: process.env.ACCESS_KEY_ID,
@@ -90,5 +89,3 @@ const uploadToR2 = (bucketName, fileName, fileContent) => {
         }
     });
 };*/
-
-
--- server.js
+++ server.js
@@ -1,59 +1,65 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+//import express, { Request, Response } from "express";
+
+if (!process.env.D1_DATABASE_URL) {
+    throw new Error("Environment variable D1_DATABASE_URL is not set.");
+}
+
+
 //const express = require("express");
 const cors = require("cors"); // Import the CORS package
 const fs = require("fs");
 const path = require("path");
 const AWS = require("aws-sdk");
-const { D1Database } = require("@cloudflare/d1");
+const {D1Database} = require("@cloudflare/d1");
+const express = require("express");
 require("dotenv").config();
-const app = (0, express_1.default)();
+
+const app = express();
 app.use(cors()); // Enable CORS for all routes
-app.use(express_1.default.json()); // Middleware to parse JSON data
-const db = new D1Database(process.env.D1_DATABASE_URL);
+app.use(express.json()); // Middleware to parse JSON data
+
+const db = new D1Database(process.env.D1_DATABASE_URL)
 // Root route for testing
-app.get("/", (req, res) => {
+app.get("/", (req , res ) => {
     res.send("Backend is up and running!");
 });
+
 // Endpoint to save quiz results
-app.post("/save-quiz", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post("/save-quiz", async (req, res) => {
     const { email, score, time } = req.body;
-    if (!email || typeof score !== "number" || typeof time !== "number") {
-        res.status(400).send("Invalid data.");
-        return;
+
+    if (!email || typeof score !== "number" || typeof time !== "number") {  
+        return res.status(400).send("Invalid data.");;
     }
-    const sqlQuery = `
+   /* const sqlQuery = `
     INSERT INTO quiz_diario_sds (email, score, time)
     VALUES (?, ?, ?);
 `;
+
     try {
-        yield db.prepare(sqlQuery).bind(email, score, time).run();
+        await db.prepare(sqlQuery).bind(email, score, time).run();
         console.log("Data saved successfully!");
         res.status(200).send("Data saved successfully!");
-    }
-    catch (err) {
+    } catch (err) {
         console.error("Error saving data to D1 database:", err);
         res.status(500).send("Failed to save data.");
-    }
-    /* fileName = `quiz_${new Date().toISOString().split("T")[0]}.csv`;
+    }*/
+
+
+    fileName = `quiz_${new Date().toISOString().split("T")[0]}.csv`;
     const filePath = path.join(__dirname, fileName);
+     // Log whether the file exists
+    if (!fs.existsSync(filePath)) {
+        console.log(`Creating new file: ${fileName}`);
+    } else {
+        console.log(`Appending to existing file: ${fileName}`);
+    }
 
     const row = `${email},${score},${time}\n`;
 
-    fs.appendFile(filePath, row, (err) => {
+
+
+    fs.appendFile(filePath, row,{ flag: "a" }, (err) => {
         if (err) {
             console.error("Error writing to CSV file:", err);
             return res.status(500).send("Failed to save data.");
@@ -62,14 +68,36 @@ app.post("/save-quiz", (req, res) => __awaiter(void 0, void 0, void 0, function*
         uploadToR2("quizzes-sds", fileName, fileContent);
         console.log("Data saved successfully!");
         res.status(200).send("Data saved successfully!");
-    });*/
-}));
+    });
+
+    fn2 = `quiz_${new Date().toISOString().split("T")[0]}_${Date.now()}.csv`;
+    const fp2 = path.join(__dirname, fn2);
+    if (!fs.existsSync(fp2)) {
+        console.log(`Creating new file: ${fn2}`);
+    } else {
+        console.log(`Appending to existing file: ${fn2}`);
+    }
+
+    fs.appendFile(fp2, row,{ flag: "a" }, (err) => {
+        if (err) {
+            console.error("Error writing to CSV file:", err);
+            return res.status(500).send("Failed to save data.");
+        }
+        const fileContent = fs.readFileSync(fp2);
+        uploadToR2("quizzes-sds", fn2, fileContent);
+        console.log("Data saved successfully!");
+        res.status(200).send("Data saved successfully!");
+    });
+
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-/*const s3 = new AWS.S3({
+
+const s3 = new AWS.S3({
     endpoint: "https://1d5bedc6e06a996c3ca93b5e69f491de.r2.cloudflarestorage.com/quizzes-sds",
     accessKeyId: process.env.ACCESS_KEY_ID,
     secretAccessKey: process.env.SECRET_ACCESS_KEY_ID,
@@ -88,4 +116,6 @@ const uploadToR2 = (bucketName, fileName, fileContent) => {
             console.log("File uploaded successfully:", data.Location);
         }
     });
-};*/
+};
+
+
